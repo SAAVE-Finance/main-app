183e290cde8aba35e65de7037e20010f8e07c253
Pratham <prathamsinghal2003@gmail.com>
2023-02-18 09:29:31 +0530

    :rocket: Completed minimal deposit modal

--- components/Buttons/deposit.tsx
+++ components/Buttons/deposit.tsx
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
+import SaaveABI from "../../abis/abi.json";
 import Typography from "@mui/material/Typography";
+import { useContractReads } from "wagmi";
+import DepositModal from "@/components/Modal/DepositModal";
+import { BigNumberish, ethers } from "ethers";
+
 const Deposit = () => {
+  const [walletUSDCBalance, setWalletUSDCBalance] = useState("0");
+  const [walletUSDTBalance, setWalletUSDTBalance] = useState("0");
+  const [walletDAIBalance, setWalletDAIBalance] = useState("0");
+
+  const saaveContract = {
+    address: "0xBFbA56f89f343dc43B99Dcb0c6e30045d2D2E948",
+    abi: SaaveABI,
+  };
+  const { data, isError, isLoading } = useContractReads({
+    contracts: [
+      {
+        ...saaveContract,
+        functionName: "getWalletUSDCBalance",
+      },
+      {
+        ...saaveContract,
+        functionName: "getWalletUSDTBalance",
+      },
+      {
+        ...saaveContract,
+        functionName: "getWalletDAIBalance",
+      },
+    ],
+    async onSuccess(data: [BigNumberish, BigNumberish, BigNumberish]) {
+      var totalDeposited = ethers.utils.formatEther(data[0]);
+      setWalletUSDCBalance(totalDeposited);
+      var totalEarned = ethers.utils.formatEther(data[1]);
+      setWalletUSDTBalance(totalEarned);
+      var totalEarned = ethers.utils.formatEther(data[2]);
+      setWalletDAIBalance(totalEarned);
+    },
+  });
   const handleClick = () => {
     setOpen(true);
   };
@@ -39,23 +76,53 @@ const Deposit = () => {
           >
             Deposit
           </Typography>
-          <div className="flex flex-col align-middle justify-center text-white">
-            <input
-              placeholder="Amount of USDC"
-              type="number"
-              className="p-4 rounded border-none bg-[#282640] my-2 "
-            />
-            <input
-              placeholder="Amount of USDT"
-              type="number"
-              className="p-4 rounded border-none bg-[#282640] my-2"
-            />
-            <input
-              placeholder="Amount of DAI"
-              type="number"
-              className="p-4 rounded border-none bg-[#282640] my-2"
-            />
-            <button className="p-4 bg-[#ff4747] my-3 rounded">Submit</button>
+          <div className="flex">
+            <DepositModal />
+            {/* <div className="flex flex-col align-middle justify-center text-white">
+              <input
+                placeholder="Amount of USDC"
+                type="number"
+                className="p-4 rounded border-none bg-[#282640] my-2 "
+                value={depositUSDCValue}
+                onChange={(e) => setDepositUSDCValue(e.target.value)}
+              />
+              {!(depositUSDCValue === "0") && (
+                <button className="p-3 bg-[#77f177]">Approve</button>
+              )}
+              <input
+                placeholder="Amount of USDT"
+                type="number"
+                className="p-4 rounded border-none bg-[#282640] my-2"
+                value={depositUSDTValue}
+                onChange={(e) => setDepositUSDTValue(e.target.value)}
+              />
+              {!(depositUSDTValue === "0") && (
+                <button className="p-3 bg-[#77f177]">Approve</button>
+              )}
+              <input
+                placeholder="Amount of DAI"
+                type="number"
+                className="p-4 rounded border-none bg-[#282640] my-2"
+                value={depositDAIValue}
+                onChange={(e) => setDepositDAIValue(e.target.value)}
+              />
+              {!(depositDAIValue === "0") && (
+                <button className="p-3 bg-[#77f177]">Approve</button>
+              )}
+              <button className="p-4 bg-[#ff4747] my-3 rounded">Submit</button>
+            </div> */}
+            <div className="flex flex-col border-l-2 border-black mx-2">
+              <h2 className="m-3">Wallet Balance</h2>
+              <h2 className="text-xl font-bold m-3 ">
+                USDC - {walletUSDCBalance}
+              </h2>
+              <h2 className="text-xl font-bold m-3 ">
+                USDT - {walletUSDTBalance}
+              </h2>
+              <h2 className="text-xl font-bold m-3 ">
+                DAI - {walletDAIBalance}
+              </h2>
+            </div>
           </div>
         </Box>
       </Modal>

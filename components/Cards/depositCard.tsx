54b82c885d6811f5c1af798223afcbf684eaf5fe
RITEKROUNAK <ritekrounak1@gmail.com>
2023-02-19 23:32:40 +0530

    Button Color Changed

--- components/Cards/depositCard.tsx
+++ components/Cards/depositCard.tsx
@@ -136,7 +136,7 @@ const Saave = () => {
                 {!(depositDAIValue === "0") && (
                   <button className="p-3 bg-[#77f177]">Approve</button>
                 )}
-                <button className="p-4 bg-[#ff4747] my-3 rounded">
+                <button className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 rounded-lg text-xl md:text-3xl font-bold px-5 py-2.5 text-center mr-2 mb-2">
                   Submit
                 </button>
               </div>

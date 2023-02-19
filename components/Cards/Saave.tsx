5f113f8279a15e2c5c817f1edbcc28bac0829b1c
RITEKROUNAK <ritekrounak1@gmail.com>
2023-02-19 23:28:59 +0530

    Home Page Card Heading

--- components/Cards/Saave.tsx
+++ components/Cards/Saave.tsx
@@ -31,7 +31,7 @@ function Saave() {
                     SAAVE
                 </div>
 
-                <h2 className="text-transparent bg-clip-text animate-text bg-gradient-to-r from-white to-[rgba(255,255,255,0.55)] text-3xl text-center  mx-auto">What are you looking to do today ?</h2>
+                <h2 className="text-transparent bg-clip-text animate-text bg-gradient-to-r from-white to-[rgba(255,255,255,0.55)] text-3xl text-center font-bold mx-auto">What are you looking to do today ?</h2>
                 <div className='flex-col sm:flex-row flex justify-around p-4 mt-4'>
                     <button type="button" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 rounded-lg text-xl md:text-3xl font-bold px-5 py-2.5 text-center mr-2 mb-2" onClick={() => router.push('/deposit')}>DEPOSIT</button>
                     <button type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 rounded-lg text-xl md:text-3xl font-bold px-5 py-2.5 text-center mr-2 mb-2" onClick={() => router.push('/withdraw')}>WITHDRAW</button>
